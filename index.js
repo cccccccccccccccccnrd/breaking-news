41d0d10174eee0a97eb1ea5eb761df5dcb1feb47
cnrd <hello@conradweise.com>
2020-03-23 13:06:11 +0100

    added ir, cn

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ const express = require('express')
 const WebSocket = require('ws')
 const puppeteer = require('puppeteer')
 
+const term = 'covid'
+
 const list = [{
-  url: 'https://www.zeit.de/suche/index?q=covid',
+  url: `https://www.zeit.de/suche/index?q=${ term }`,
   selector: '.zon-teaser-standard__title'
 }, {
-  url: 'https://www.brigitte.de/search?q=covid',
+  url: `https://www.brigitte.de/search?q=${ term }`,
   selector: '.a-headline-teaser'
 }, {
-  url: 'https://www.positanonews.it/?s=covid&post_type=post',
+  url: `https://www.positanonews.it/?s=${ term }&post_type=post`,
   selector: '.news-item h1 a'
 }, {
-  url: 'https://www.der-postillon.com/search?q=covid',
+  url: `https://www.der-postillon.com/search?q=${ term }`,
   selector: '.post-title.entry-title a'
 }, {
-  url: 'https://www.vice.com/en_us/search?q=covid',
+  url: `https://www.vice.com/en_us/search?q=${ term }`,
   selector: '.heading'
+}, {
+  url: `https://www.irinn.ir/fa/search?query=${ term }`,
+  selector: '.search_result_container .tag_title'
+}, {
+  url: `https://search.cctv.com/search.php?qtext=${ term }&type=web`,
+  selector: '.tuwenjg h3'
 }]
 
 const state = {}
@@ -35,31 +43,47 @@ wss.on('connection', (ws) => {
   ws.send(JSON.stringify(state))
 })
 
+function broadcast () {
+  console.log(state)
+
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(state))
+    }
+  })
+}
+
 async function go (browser, url, selector) {
   console.log(url, selector)
-  const page = await browser.newPage()
-  page.setDefaultNavigationTimeout(0)
-  await page.goto(url, { waitUntil: 'networkidle2' })
-  await page.waitForSelector(selector)
 
-  const data = await page.evaluate((selector) => {
-    const headings = Array.from(document.querySelectorAll(selector))
-    return headings.map(h => h.innerText)
-  }, selector)
-
-  return data
+  try {
+    const page = await browser.newPage()
+    page.setDefaultNavigationTimeout(0)
+    await page.goto(url, { waitUntil: 'networkidle2' })
+    await page.waitForSelector(selector)
+  
+    const data = await page.evaluate((selector) => {
+      const headings = Array.from(document.querySelectorAll(selector))
+      return headings.map(h => h.innerText)
+    }, selector)
+  
+    page.close()
+    return data
+  } catch(error) {
+    console.log(error)
+  }
 }
 
 async function init () {
-  const browser = await puppeteer.launch(/* {headless: false} */)
+  const browser = await puppeteer.launch({ headless: false })
 
   for (const entry of list) {
     const headings = await go(browser, entry.url, entry.selector)
     state[entry.url] = headings
   }
 
-  console.log(state)
   await browser.close()
+  broadcast()
 }
 
 init()
