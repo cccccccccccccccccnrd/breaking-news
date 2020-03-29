1f0500976edf3fe499a4086b8155125f29f20028
cnrd <hello@conradweise.com>
2020-03-30 00:43:33 +0200

    no log pls

--- index.js
+++ index.js
@@ -235,7 +235,6 @@ async function check () {
   try {
     for (const entry of list) {
       const headings = await go(browser, entry.url, entry.selector)
-      console.log(headings)
       state.raw[entry.url] = headings
     }
 
