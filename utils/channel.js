b0f2d7a817d8109262d7117c638590490cfdcbf5
cnrd <hello@conradweise.com>
2020-04-01 12:26:44 +0200

    fixi

--- utils/channel.js
+++ utils/channel.js
@@ -30,7 +30,7 @@ bot.start((ctx) => {
 bot.launch()
 
 function headline (headline) {
-  if (context) {
+  if (context && headline.title) {
     /* context.telegram.sendMessage('@infodemiclive', `<a href="${ headline.url }">${ headline.title }</a>`, { parse_mode: 'html' }) */
     context.telegram.sendMessage('@infodemiclive', headline.title)
   } else {
