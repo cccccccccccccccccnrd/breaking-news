49d452ee8331c0cc14a766a865b173e038c03f74
cnrd <hello@conradweise.com>
2020-04-03 12:41:16 +0200

    ;

--- index.js
+++ index.js
@@ -318,8 +318,8 @@ function flatten (object) {
 
 function shuffle(actual) {
   for (let i = actual.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    [actual[i], actual[j]] = [actual[j], actual[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [actual[i], actual[j]] = [actual[j], actual[i]]
   }
 
   return actual
